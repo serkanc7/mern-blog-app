c9fa31ba66bfff8aa7b477a4be0733ed76bfb259
Serkan <srknclk98@gmail.com>
2024-07-15 21:29:29 +0300

    feat: Set up MongoDB connection

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
